67eedb125b93ad2522ba19885caebe0054dc47c3
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-16 11:43:05 -0800

    Tested chart on index.html

--- public/scripts/views/dashboardView.js
+++ public/scripts/views/dashboardView.js
@@ -1,1 +1,17 @@
 // From signin landing page, option to stay signed in makes dashboard = landing
+let ctx = document.getElementById('chart_canvas');
+
+$.ajax(
+
+);
+
+let wkly_summary_chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: [ 'first', 'second', 'third' ],
+    datasets: [{
+      label: 'stuff',
+      data: [ 2, 9, 5 ]
+    }]
+  }
+});
