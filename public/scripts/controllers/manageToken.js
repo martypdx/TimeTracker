9593fda79df60158c6c962cb15c9affc9597c36f
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-17 16:42:45 -0800

    Fixed bad paste in manageToken.js

--- public/scripts/controllers/manageToken.js
+++ public/scripts/controllers/manageToken.js
@@ -8,21 +8,7 @@
   manageToken.getToken = () => {
     if (!ttToken) ttToken = JSON.parse(localStorage.getItem('timetrackertoken'));
     return ttToken;
-  };(function(module) {
-//TODO: Add to index.html
-
-    const manageToken = {};
-  
-    let ttToken; 
-
-    manageToken.getToken = () => {
-      if (!ttToken) ttToken = JSON.parse(localStorage.getItem('timetrackertoken'));
-      return ttToken;
-    };
-  
-    module.manageToken = manageToken;
-
-  })(window);
+  };
   
   module.manageToken = manageToken;
 
