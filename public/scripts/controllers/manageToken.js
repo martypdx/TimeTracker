b6da9e9047122e61d1c3e2db257dd6c4b1476a76
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-17 18:35:23 -0800

    Got graph working and fixed HTML close tags (had nesting problems); fixed rendering of graphs

--- public/scripts/controllers/manageToken.js
+++ public/scripts/controllers/manageToken.js
@@ -6,7 +6,8 @@
   let ttToken; 
 
   manageToken.getToken = () => {
-    if (!ttToken) ttToken = JSON.parse(localStorage.getItem('timetrackertoken'));
+    // if (!ttToken) ttToken = JSON.parse(localStorage.getItem('timetrackertoken'));
+    if (!ttToken) ttToken = localStorage.getItem('timetrackertoken');
     return ttToken;
   };
   
